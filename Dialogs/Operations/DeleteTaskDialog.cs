d422b46255f1ac489c77f4a70d7ddee609f61d91
JD Bots <info@jd-bots.com>
2021-04-03 23:34:00 +0530

    Add project files.

---
+++ Dialogs/Operations/DeleteTaskDialog.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoBot.Dialogs.Operations
+{
+    public class DeleteTaskDialog : ComponentDialog
+    {
+        public DeleteTaskDialog() : base(nameof(DeleteTaskDialog))
+        {
+            var waterfallSteps = new WaterfallStep[]
+            {
+
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+    }
+}
---
+++ Dialogs/Operations/ViewTaskDialog.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoBot.Dialogs.Operations
+{
+    public class ViewTaskDialog : ComponentDialog
+    {
+        public ViewTaskDialog() : base(nameof(ViewTaskDialog))
+        {
+            var waterfallSteps = new WaterfallStep[]
+            {
+
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+    }
+}
